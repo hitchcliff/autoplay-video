7ba4a985f9831cef102e4bd54d1e2227d3524829
kevin700brands <kevin@700brands.com>
2022-08-31 07:21:45 +0800

    fix bugs

--- index.js
+++ index.js
@@ -9,54 +9,59 @@ class AutoplayVideo {
   _src;
   _videoEl;
   _imgEl;
+  _screenWidth;
 
   play() {
+    this._screenWidth = document.documentElement.clientWidth;
+    this._videoEl = document.getElementById(this.videoPlayerId);
+
     // Watch screen on resize
+    this._showViewport();
     this._onResize();
 
-    this._videoEl = document.getElementById(this.videoPlayerId);
     this._videoEl.src = this._src;
+    this._videoEl.play();
 
-    this._delectIfPaused(); // Detects the video if paused
     this._styleVideo(); // Styles the Video
+    this._delectIfPaused();
   }
 
   _styleVideo() {
     const style = this._videoEl.style;
 
-    style.height = "100vh";
     style.width = "100%";
     style.objectFit = "cover";
     style.objectPosition = "top";
   }
 
   _delectIfPaused() {
-    // this._videoEl.pause(); // toggle to see GIF
-    if (this._videoEl.played && window.innerWidth >= 768) return;
+    if (this._videoEl.paused && this._screenWidth <= 980) {
+      // console.log(this._videoEl.paused);
+      this._img = document.createElement("img");
+      this._img.className = "autoplay-bg-video";
+      this._img.src = this.srcImg;
 
-    this._img = document.createElement("img");
-    this._img.className = "autoplay-bg-video";
-    this._img.src = this.srcImg;
-
-    this._videoEl.parentNode.replaceChild(this._img, this._videoEl);
+      this._videoEl.parentNode.replaceChild(this._img, this._videoEl);
+    }
   }
 
   _onResize() {
-    if (window.innerWidth >= 768) {
+    window.addEventListener("resize", () => {
+      this._screenWidth = document.documentElement.clientWidth;
+
+      this._showViewport();
+    });
+  }
+
+  _showViewport() {
+    // Assign value to screen width
+    if (this._screenWidth >= 980) {
       this._src = this.srcDesktopMp4;
     } else {
       this._src = this.srcMobileMp4;
     }
 
-    window.addEventListener("resize", () => {
-      if (window.innerWidth >= 768) {
-        this._src = this.srcDesktopMp4;
-      } else {
-        this._src = this.srcMobileMp4;
-      }
-
-      // Sets the src on resize
-      this._videoEl.src = this._src;
-    });
+    // Assign sign
+    this._videoEl.src = this._src;
   }
 }
